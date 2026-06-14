08d9d621fdc71c1ad4b59f9e5bbd3b841a0eadcd
georgeeduardtp <georgeeduard.tp@gmail.com>
2026-06-14 21:36:50 +0200

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,13 +37,17 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       { url: '/favicon.ico' },
-      { url: '/pelugo512x512.png', sizes: '512x512', type: 'image/png' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/android-chrome-192x192.png', sizes: '192x192', type: 'image/png' },
+      { url: '/android-chrome-512x512.png', sizes: '512x512', type: 'image/png' },
     ],
     apple: [
-      { url: '/pelugo512x512.png', sizes: '180x180', type: 'image/png' },
+      { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
     ],
     shortcut: ['/favicon.ico'],
   },
+  manifest: '/site.webmanifest',
   openGraph: {
     type: 'website',
     locale: 'es_ES',
